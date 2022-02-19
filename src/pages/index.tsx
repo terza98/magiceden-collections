2294272a0b8ab8ffba61be3f595ce51569ee5be3
terza98 <terza98@hotmail.rs>
2022-02-19 16:46:10 +0100

    render page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,73 @@
 import {
-  Link as ChakraLink,
+  Link,
   Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+  Box,
+  InputGroup,
+  Input,
+  Button,
+  Flex,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { useEffect, useState } from "react";
+import {
+  getCollectionFromHowrare,
+  getCollectionFromMagiceden,
+} from "../api/queries";
+import Loading from "../components/Loading";
+import { sortByRarity } from "../utils/helpers";
+import { TableWithSearch } from "../components/TableWithSearch/TableWithSearch";
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+const Index = () => {
+  const [loading, setLoading] = useState<boolean>(false);
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+  const [listings, setListings] = useState([]);
+  const [collectionName, setCollectionName] = useState<string>("");
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+  // useEffect(() => {
+  //   getCollectionFromMagiceden("blockstars");
+  // }, []);
+  const searchCollection = (): void => {
+    setLoading(true);
+    getCollectionFromHowrare(collectionName).then((howrare) => {
+      getCollectionFromMagiceden(collectionName).then((magiceden) => {
+        console.log(magiceden);
+        console.log(howrare);
+        setListings(sortByRarity(howrare, magiceden));
+      });
+      setLoading(false);
+    });
+  };
+  return (
+    <Box p={10}>
+      <Heading size="lg" mb={10}>
+        Collections rarity - returns last 20 listed by rarity descending
+      </Heading>
+      <InputGroup>
+        <Input
+          border="1px solid"
+          maxW="300px"
+          value={collectionName}
+          placeholder="Enter collection name: eg. blockstars"
+          onChange={(e) => setCollectionName(e.target.value)}
+        ></Input>
+        <Button onClick={searchCollection}>Search</Button>
+      </InputGroup>
+      <Loading loading={loading} />
+      <Flex flexDir="column" mt={10}>
+        {listings.map((item) => (
+          <Flex key={item.id}>
+            <Text>{item.id}</Text>
+            <Image src={item.image} width="150px" height="150px" />
+            <Text>{item.price}</Text>
+          </Flex>
+        ))}
+      </Flex>
+      <TableWithSearch />
+      <DarkModeSwitch />
+    </Box>
+  );
+};
+export default Index;
